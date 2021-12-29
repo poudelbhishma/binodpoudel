5492ee2b43349a88b3d62238590f1745473d49fa
poudelbhishma <94956081+poudelbhishma@users.noreply.github.com>
2021-12-29 19:38:28 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>hotel</title>
+  <link rel="stylesheet" type="text/css" href="style_1.css">
+
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,400;1,100;1,400&display=swap" rel="stylesheet">
+
+<script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+
+
+
+</head>
+<body>
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Hotel Balaji </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="service.php">Servies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="blog.php">Our Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="order.php">Order</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      
+    
+  </div>
+</nav>
+
+</header>
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/hotel1.jpg" alt="Kathmandu" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Kathmandu</h3>
+        <p>Thank you vivst our Kathmandu Hotel.</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/hotel2.jpg" alt="Pokhara" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Pokhara</h3>
+        <p>Welcome to, Pokhara</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/hotel3.jpg" alt="Butwal" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Butwal</h3>
+        <p>We are always for yours.</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5" id="about">
+  <div class="py-5">
+    <h3 class="text-center">About Us</h3>
+  </div>
+
+
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-66 col-12">
+      <img src="images/bed3.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-66 col-12">
+      <h2 class="display-4">Thank You  For Visit My Hotel </h2>
+      <p class="py-3">Most hotels around the world are coming up with slogans and taglines to get the attention of potential customers. They also are looking to get positive first impressions based on their image so that people will associate them with quality services. Moreover, a solid slogan can help any operation to distinguish itself from any competitor.</p>
+      <a href="about.php" class="btn btn-outline-success">For More Info</a>
+    </div>
+  </div>
+</div>
+</section>
+
+<section class="my-5" id="service">
+  <div class="py-5">
+    <h3 class="text-center">Services</h3>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/binod.jpg" alt="Card image">
+            <div class="card-body">
+            <h4 class="card-title">Party Hall</h4>
+            <p class="card-text">We have a beautifull different party hole. </p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+        </div>
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/card2.jpg" alt="Card image">
+            <div class="card-body">
+            <h4 class="card-title">services Women</h4>
+            <p class="card-text">Awesome fast and good services.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+        </div>
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/card3.png" alt="Card image">
+            <div class="card-body">
+            <h4 class="card-title">Delivery Services</h4>
+            <p class="card-text">Our services is in the city.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h3 class="text-center">Our Pictures gallery</h3>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img1.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img2.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img3.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img4.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img5.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img6.jpg" class="img-fluid pb-4">
+        </div>
+
+      </div>
+    </div>
+</section>
+
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h3 class="text-center">Join Us </h3>
+  </div>
+  <div class="w-50 m-auto">
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" autocomplete="off" placeholder="Enter your full-name" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Emal i'd</label>
+        <input type="text" name="email" autocomplete="off" placeholder="Enter your email-id" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Mobile</label>
+        <input type="text" name="mobile" autocomplete="off" placeholder="Enter your Mobile number" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Comment</label>
+        <textarea class="form-control" placeholder="write something :) " name="comment"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">submit</button>
+    </form>
+  </div>
+</section>
+
+
+<!-- Start Contact info -->
+  <div class="contact-imfo-box">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 arrow-right">
+          <i class="fa fa-volume-control-phone"></i>
+          <div class="overflow-hidden">
+            <h4>Phone</h4>
+            <p class="lead">
+              +977-9811451715
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 arrow-right">
+          <i class="fa fa-envelope"></i>
+          <div class="overflow-hidden">
+            <h4>Email</h4>
+            <p class="lead">
+              devjerry@gmail.com
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <i class="fa fa-map-marker"></i>
+          <div class="overflow-hidden">
+            <h4>Location</h4>
+            <p class="lead">
+              New-Road,Kathamandu.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Contact info -->
+<!-- Start Footer -->
+  <footer class="footer-area bg-f">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <h3>About Us</h3>
+          <p>Integer cursus scelerisque ipsum id efficitur. Donec a dui fringilla, gravida lorem ac, semper magna. Aenean rhoncus ac lectus a interdum. Vivamus semper posuere dui.</p>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <h3>Contact US</h3>
+          <div class="subscribe_form">
+            <form class="subscribe_form">
+              <input name="text" id="subs-email" class="form_input" placeholder="Write something..." type="text">
+              <button type="submit" class="submit">Send</button>
+              <div class="clearfix"></div>
+            </form>
+          </div>
+          <ul class="list-inline f-social">
+            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <h3>Contact information</h3>
+          <p class="lead">Shivraj-04,Kapilvastu,Lumbini,Nepal.</p>
+          <p class="lead"><a href="#">+977 523 145 777</a></p>
+          <p><a href="#">customer@gmail.com</a></p>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <h3>Opening hours</h3>
+          <p><span class="text-color">Monday: </span>  8:AM - 12:Pm</p>
+          <p><span class="text-color">Tue-Wed :</span> 9:Am - 10:PM</p>
+          <p><span class="text-color">Thu-Fri :</span> 8:Am - 8:PM</p>
+          <p><span class="text-color">Sat-Sun :</span> 5:PM - 10:PM</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <p class="company-name">All Rights Reserved. &copy; 1998 <a href="#">Balaji Hotel & Resturent</a> Pro. : 
+          <a href="https://html.design/">Bhishma Poudel</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </footer>
+  <!-- End Footer -->
+  
+
+
+
+<!-- scroll top button  -->
+<a href="#home" class="fas fa-angle-up" id="scroll-top"></a>
+
+
+
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
